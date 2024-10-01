cc9aaeaaa2a330af65a68bab9d145d2779e6591b
Valeria Sergeieva <leravanna@gmail.com>
2024-10-01 17:08:31 +0500

    vite + diffuse reflection

---
+++ src/main.ts
@@ -0,0 +1,155 @@
+import { renderScene } from './raytracing';
+import { Sphere, Vector, Color, Light } from './raytracing';
+
+let spheres = [
+    new Sphere(new Vector(0, -3, 4), 2, new Color(255, 0, 0)),
+    new Sphere(new Vector(-2, 0, 4), 1, new Color(0, 255, 0)),
+    new Sphere(new Vector(2, 0, 4), 1, new Color(0, 0, 255)),
+];
+
+let lights = [
+    new Light("ambient", 0.2),
+    new Light("point", 0.6, new Vector(2, 1, 0)),
+    new Light("directional", 0.2, undefined, new Vector(1, 4, 4)),
+];
+
+const scene = {
+    spheres: spheres,
+    lights: lights
+};
+
+function updateSpheresList() {
+    const spheresContainer = document.getElementById("spheresContainer")!;
+    spheresContainer.innerHTML = ""; // Очищаем контейнер
+
+    scene.spheres.forEach((sphere, index) => {
+        const sphereElement = document.createElement("div");
+        sphereElement.innerHTML = `
+<div class="hContainer">
+
+      <p><strong>${index + 1}</strong></p>
+      <label>X: <input type="number" id="sphere-center-x-${index}" value="${sphere.center.x}"></label>
+      <label>Y: <input type="number" id="sphere-center-y-${index}" value="${sphere.center.y}"></label>
+      <label>Z: <input type="number" id="sphere-center-z-${index}" value="${sphere.center.z}"></label>
+      <label>Radius: <input type="number" id="sphere-radius-${index}" value="${sphere.radius}"></label>
+      <label>R: <input type="number" id="sphere-color-r-${index}" value="${sphere.color.r}" max="255" min="0"></label>
+      <label>G: <input type="number" id="sphere-color-g-${index}" value="${sphere.color.g}" max="255" min="0"></label>
+      <label>B: <input type="number" id="sphere-color-b-${index}" value="${sphere.color.b}" max="255" min="0"></label>
+      <button id="delete-sphere-${index}">X</button>
+      </div>
+    `;
+        spheresContainer.appendChild(sphereElement);
+
+        (document.getElementById(`sphere-center-x-${index}`) as HTMLInputElement).addEventListener("input", () => updateSphere(index));
+        (document.getElementById(`sphere-center-y-${index}`) as HTMLInputElement).addEventListener("input", () => updateSphere(index));
+        (document.getElementById(`sphere-center-z-${index}`) as HTMLInputElement).addEventListener("input", () => updateSphere(index));
+        (document.getElementById(`sphere-radius-${index}`) as HTMLInputElement).addEventListener("input", () => updateSphere(index));
+        (document.getElementById(`sphere-color-r-${index}`) as HTMLInputElement).addEventListener("input", () => updateSphere(index));
+        (document.getElementById(`sphere-color-g-${index}`) as HTMLInputElement).addEventListener("input", () => updateSphere(index));
+        (document.getElementById(`sphere-color-b-${index}`) as HTMLInputElement).addEventListener("input", () => updateSphere(index));
+
+        (document.getElementById(`delete-sphere-${index}`) as HTMLButtonElement).addEventListener("click", () => {
+            deleteSphere(index);
+        });
+    });
+}
+
+function updateLightsList() {
+    const lightsContainer = document.getElementById("lightsContainer")!;
+    lightsContainer.innerHTML = "";
+
+    scene.lights.forEach((light, index) => {
+        const lightElement = document.createElement("div");
+        lightElement.innerHTML = `
+<div class="hContainer">
+      <p><strong>${light.type}</strong></p>
+      <label>Int: <input type="number" id="light-intensity-${index}" value="${light.intensity}" step="0.1"></label>
+      <label>X: <input type="number" id="light-position-x-${index}" value="${light.position?.x || ''}"></label>
+      <label>Y: <input type="number" id="light-position-y-${index}" value="${light.position?.y || ''}"></label>
+      <label>Z: <input type="number" id="light-position-z-${index}" value="${light.position?.z || ''}"></label>
+      <button id="delete-light-${index}">X</button></div>
+    `;
+        lightsContainer.appendChild(lightElement);
+
+        (document.getElementById(`light-intensity-${index}`) as HTMLInputElement).addEventListener("input", () => updateLight(index));
+        (document.getElementById(`light-position-x-${index}`) as HTMLInputElement).addEventListener("input", () => updateLight(index));
+        (document.getElementById(`light-position-y-${index}`) as HTMLInputElement).addEventListener("input", () => updateLight(index));
+        (document.getElementById(`light-position-z-${index}`) as HTMLInputElement).addEventListener("input", () => updateLight(index));
+
+        (document.getElementById(`delete-light-${index}`) as HTMLButtonElement).addEventListener("click", () => {
+            deleteLight(index);
+        });
+    });
+}
+
+document.getElementById("addSphereButton")?.addEventListener("click", () => {
+    const x = parseFloat((document.getElementById("newSphereX") as HTMLInputElement).value);
+    const y = parseFloat((document.getElementById("newSphereY") as HTMLInputElement).value);
+    const z = parseFloat((document.getElementById("newSphereZ") as HTMLInputElement).value);
+    const radius = parseFloat((document.getElementById("newSphereRadius") as HTMLInputElement).value);
+    const r = parseInt((document.getElementById("newSphereR") as HTMLInputElement).value);
+    const g = parseInt((document.getElementById("newSphereG") as HTMLInputElement).value);
+    const b = parseInt((document.getElementById("newSphereB") as HTMLInputElement).value);
+
+    const newSphere = new Sphere(new Vector(x, y, z), radius, new Color(r, g, b));
+    scene.spheres.push(newSphere);
+    updateSpheresList();
+    renderScene(scene);
+});
+
+// Добавление нового источника света
+document.getElementById("addLightButton")?.addEventListener("click", () => {
+    const type = (document.getElementById("newLightType") as HTMLSelectElement).value;
+    const intensity = parseFloat((document.getElementById("newLightIntensity") as HTMLInputElement).value);
+    const x = parseFloat((document.getElementById("newLightX") as HTMLInputElement).value);
+    const y = parseFloat((document.getElementById("newLightY") as HTMLInputElement).value);
+    const z = parseFloat((document.getElementById("newLightZ") as HTMLInputElement).value);
+
+    const newLight = new Light(type, intensity, type !== 'ambient' ? new Vector(x, y, z) : undefined, type === 'point' ? new Vector(x, y, z) : undefined);
+    scene.lights.push(newLight);
+    updateLightsList();
+    renderScene(scene);
+});
+
+// Функция для обновления параметров сферы
+function updateSphere(index: number) {
+    const sphere = scene.spheres[index];
+    sphere.center.x = parseFloat((document.getElementById(`sphere-center-x-${index}`) as HTMLInputElement).value);
+    sphere.center.y = parseFloat((document.getElementById(`sphere-center-y-${index}`) as HTMLInputElement).value);
+    sphere.center.z = parseFloat((document.getElementById(`sphere-center-z-${index}`) as HTMLInputElement).value);
+    sphere.radius = parseFloat((document.getElementById(`sphere-radius-${index}`) as HTMLInputElement).value);
+    sphere.color.r = parseInt((document.getElementById(`sphere-color-r-${index}`) as HTMLInputElement).value);
+    sphere.color.g = parseInt((document.getElementById(`sphere-color-g-${index}`) as HTMLInputElement).value);
+    sphere.color.b = parseInt((document.getElementById(`sphere-color-b-${index}`) as HTMLInputElement).value);
+
+    renderScene(scene);
+}
+
+// Функция для обновления параметров освещения
+function updateLight(index: number) {
+    const light = scene.lights[index];
+    light.intensity = parseFloat((document.getElementById(`light-intensity-${index}`) as HTMLInputElement).value);
+    if (light.position) {
+        light.position.x = parseFloat((document.getElementById(`light-position-x-${index}`) as HTMLInputElement).value);
+        light.position.y = parseFloat((document.getElementById(`light-position-y-${index}`) as HTMLInputElement).value);
+        light.position.z = parseFloat((document.getElementById(`light-position-z-${index}`) as HTMLInputElement).value);
+    }
+
+    renderScene(scene);
+}
+
+function deleteSphere(index: number) {
+    scene.spheres.splice(index, 1);
+    updateSpheresList();
+    renderScene(scene);
+}
+
+function deleteLight(index: number) {
+    scene.lights.splice(index, 1);
+    updateLightsList();
+    renderScene(scene);
+}
+
+updateSpheresList();
+updateLightsList();
+renderScene(scene);
---
+++ src/raytracing.ts
@@ -0,0 +1,210 @@
+export class Color {
+    r: number;
+    g: number;
+    b: number;
+
+    constructor(r: number, g: number, b: number) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    mul(intensity: number): Color {
+        return new Color(
+            Math.min(255, Math.max(0, this.r * intensity)),
+            Math.min(255, Math.max(0, this.g * intensity)),
+            Math.min(255, Math.max(0, this.b * intensity))
+        );
+    }
+}
+
+export class Vector {
+    x: number;
+    y: number;
+    z: number;
+
+    constructor(x: number, y: number, z: number) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    dot(vec: Vector): number {
+        return this.x * vec.x + this.y * vec.y + this.z * vec.z;
+    }
+
+    sub(vec: Vector): Vector {
+        return new Vector(this.x - vec.x, this.y - vec.y, this.z - vec.z);
+    }
+
+    mul(n: number): Vector {
+        return new Vector(this.x * n, this.y * n, this.z * n);
+    }
+
+    add(vec: Vector): Vector {
+        return new Vector(this.x + vec.x, this.y + vec.y, this.z + vec.z);
+    }
+
+    length(): number {
+        return Math.sqrt(this.dot(this));
+    }
+}
+
+export class Sphere {
+    center: Vector;
+    radius: number;
+    color: Color;
+
+    constructor(center: Vector, radius: number, color: Color) {
+        this.center = center;
+        this.radius = radius;
+        this.color = color;
+    }
+}
+
+const lightTypes = {
+    ambient: "ambient",
+    point: "point",
+    directional: "directional",
+};
+export class Light {
+    type: string;
+    intensity: number;
+    position?: Vector;
+    direction?: Vector;
+
+    constructor(type: string, intensity: number, position?: Vector, direction?: Vector) {
+        this.type = type;
+        this.intensity = intensity;
+        this.position = position;
+        this.direction = direction;
+    }
+}
+
+interface Scene {
+    spheres: Sphere[];
+    lights: Light[];
+}
+
+function calculateDiscriminant(a: number, b: number, c: number): number {
+    return b * b - 4 * a * c;
+}
+
+function solveQuadratic(a: number, b: number, discriminant: number): [number, number] {
+    const sqrtDiscriminant = Math.sqrt(discriminant);
+    const root1 = (-b + sqrtDiscriminant) / (2 * a);
+    const root2 = (-b - sqrtDiscriminant) / (2 * a);
+    return [root1, root2];
+}
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d");
+
+export function renderScene(scene: Scene) {
+    if (!ctx) return;
+
+
+    const canvasImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const viewportSize = 1;
+    const cameraPosition = new Vector(0, 0, 0);
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let x = -canvas.width / 2; x < canvas.width / 2; x++) {
+        for (let y = -canvas.height / 2; y < canvas.height / 2; y++) {
+            const direction = new Vector(
+                (x * viewportSize) / canvas.width,
+                (y * viewportSize) / canvas.height,
+                1
+            );
+            const color = traceRay(cameraPosition, direction, 1, Infinity, scene.spheres, scene.lights);
+            PutPixel(x, y, color, canvasImage);
+        }
+    }
+
+    ctx.putImageData(canvasImage, 0, 0);
+}
+
+function PutPixel(x: number, y: number, color: Color, canvasImage: ImageData): void {
+    x = canvas.width / 2 + x;
+    y = canvas.height / 2 - y - 1;
+
+    if (x < 0 || x >= canvas.width || y < 0 || y >= canvas.height) {
+        return;
+    }
+
+    const offset = 4 * (x + canvasImage.width * y);
+    canvasImage.data[offset] = color.r;
+    canvasImage.data[offset + 1] = color.g;
+    canvasImage.data[offset + 2] = color.b;
+    canvasImage.data[offset + 3] = 255;
+}
+
+function traceRay(rayOrigin: Vector, rayDirection: Vector, minT: number, maxT: number, spheres: Sphere[], lights: Light[]): Color {
+    let closestIntersectionDistance = Infinity;
+    let closestSphere: Sphere | null = null;
+
+    spheres.forEach((sphere) => {
+        const [t1, t2] = intersectRayWithSphere(rayOrigin, rayDirection, sphere);
+
+        if (t1 > minT && t1 < maxT && t1 < closestIntersectionDistance) {
+            closestIntersectionDistance = t1;
+            closestSphere = sphere;
+        }
+
+        if (t2 > minT && t2 < maxT && t2 < closestIntersectionDistance) {
+            closestIntersectionDistance = t2;
+            closestSphere = sphere;
+        }
+    });
+
+    if (!closestSphere) {
+        return new Color(255, 255, 255);
+    }
+
+    const point = rayOrigin.add(rayDirection.mul(closestIntersectionDistance));
+    let normal = point.sub((closestSphere as Sphere).center);
+    normal = normal.mul(1.0 / normal.length());
+    return (closestSphere as Sphere).color.mul(computeLighting(point, normal, lights));
+}
+
+function intersectRayWithSphere(rayOrigin: Vector, rayDirection: Vector, sphere: Sphere): [number, number] {
+    const originToCenter = rayOrigin.sub(sphere.center);
+
+    const a = rayDirection.dot(rayDirection);
+    const b = 2 * originToCenter.dot(rayDirection);
+    const c = originToCenter.dot(originToCenter) - Math.pow(sphere.radius, 2);
+
+    const discriminant = calculateDiscriminant(a, b, c);
+
+    if (discriminant < 0) {
+        return [Infinity, Infinity];
+    }
+
+    return solveQuadratic(a, b, discriminant);
+}
+
+function computeLighting(point: Vector, normal: Vector, sceneLights: Light[]): number {
+    let intensity = 0;
+    sceneLights.forEach((light) => {
+        if (light.type === lightTypes.ambient) {
+            intensity += light.intensity;
+        } else {
+            let vectorL: Vector;
+
+            if (light.type === lightTypes.point && light.position) {
+                vectorL = light.position.sub(point);
+            } else if (light.direction) {
+                vectorL = light.direction;
+            } else {
+                return;
+            }
+
+            let normalDotL = normal.dot(vectorL);
+            if (normalDotL > 0) {
+                intensity += (light.intensity * normalDotL) / (normal.length() * vectorL.length());
+            }
+        }
+    });
+    return intensity;
+}
